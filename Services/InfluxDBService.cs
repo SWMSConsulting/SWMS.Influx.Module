a9005d4fcca24d1329a137a67e77c4ec5be0e9b5
Andre Buss <buss@swms.de>
2024-06-14 14:50:48 +0200

    Added automatic calculation of Influx aggregate time

--- Services/InfluxDBService.cs
+++ Services/InfluxDBService.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using DevExpress.ExpressApp.Security;
 using InfluxDB.Client;
 using Microsoft.Extensions.Configuration;
+using SWMS.Influx.Module.Models;
 
 namespace SWMS.Influx.Module.Services
 {
@@ -36,7 +37,7 @@ namespace SWMS.Influx.Module.Services
             return await action(query);
         }
 
-        public static string FluxDurationRegexPattern = @"^(\d+w)?(\d+d)?(\d+h)?(\d+m)?(\d+s)?$";
+        public static string FluxDurationRegexPattern = @"^(\d+w)?(\d+d)?(\d+h)?(\d+m)?(\d+s)?(\d+ms)?$";
 
         public static bool IsValidFluxDuration(string duration)
         {
@@ -47,6 +48,74 @@ namespace SWMS.Influx.Module.Services
 
             Regex regex = new Regex(FluxDurationRegexPattern);
             return regex.IsMatch(duration);
+        }
+
+        public static string CalculateFluxDuration(InfluxTimeWindow influxTimeWindow, int pointNumber = 360)
+        {
+            return CalculateFluxDuration(influxTimeWindow.StartDate, influxTimeWindow.EndDate, pointNumber);
+        }
+
+        public static string CalculateFluxDuration(DateTime StartDate, DateTime EndDate, int pointNumber = 360)
+        {
+            if (StartDate >= EndDate)
+            {
+                throw new ArgumentException("The StartDate must be before the EndDate.");
+            }
+            var timeSpan = EndDate - StartDate;
+            var milliseconds = timeSpan.TotalMilliseconds;
+            var aggregateTime = milliseconds / pointNumber;
+            return MillisecondsToFluxDuration(aggregateTime);
+        }
+
+        public static string MillisecondsToFluxDuration(double milliseconds)
+        {
+            if (milliseconds <= 0)
+            {
+                throw new ArgumentException("The number of seconds must be positive.");
+            }
+
+            TimeSpan timeSpan = TimeSpan.FromMilliseconds(milliseconds);
+
+            return TimeSpanToFluxDuration(timeSpan);
+        }
+
+        public static string TimeSpanToFluxDuration(TimeSpan timeSpan)
+        {
+            int weeks = (int)(timeSpan.TotalDays / 7);
+            int days = timeSpan.Days % 7;
+            int hours = timeSpan.Hours;
+            int minutes = timeSpan.Minutes;
+            int seconds = timeSpan.Seconds;
+            int milliseconds = timeSpan.Milliseconds;
+
+            string result = "";
+
+            if (weeks > 0)
+            {
+                result += $"{weeks}w";
+            }
+            if (days > 0)
+            {
+                result += $"{days}d";
+            }
+            if (hours > 0)
+            {
+                result += $"{hours}h";
+            }
+            if (minutes > 0)
+            {
+                result += $"{minutes}m";
+            }
+            if (seconds > 0)
+            {
+                result += $"{seconds}s";
+            }
+            if (milliseconds > 0)
+            {
+                result += $"{milliseconds}ms";
+            }
+
+            return result;
         }
 
     }
